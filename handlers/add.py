0002a463ad2ca357add08902b5adfaac13f83b48
foobar <foobar@example.com>
2025-04-18 19:47:00 +0300

    База+

--- handlers/add.py
+++ handlers/add.py
@@ -15,12 +15,13 @@ class Form_task(StatesGroup):
     time = State()
     type = State()
     date = State()
+    days = State()
 
 
 @router.message(F.text == "Добавить напоминалку")
 async def handle_message(message: Message, state: FSMContext):
     user_id = message.from_user.id
-
+    await state.update_data(id=user_id)
     await state.set_state(Form_task.task)
     await message.answer('Введите вашу напоминалку', reply_markup=types.ReplyKeyboardRemove())
 
@@ -34,21 +35,62 @@ async def get_task(message: Message, state: FSMContext):
 
 @router.message(Form_task.time)
 async def get_time(message: Message, state: FSMContext):
-    await state.update_data(task=message.text)
+    await state.update_data(time=message.text)
     await state.set_state(Form_task.type)
-    await message.answer('Теперь выберите формат отправки сообщения')
+    await message.answer('Теперь выберите формат отправки сообщения')  # здесь должна быть клавиатура
 
 
 @router.message(Form_task.type)
 async def get_type(message: Message, state: FSMContext):
-    await state.update_data(task=message.text)
-    await state.set_state(Form_task.type)
-    await message.answer('Введите дни')
-
-@router.message(Form_task.type)
-async def get_type(message: Message, state: FSMContext):
-    await state.update_data(task=message.text)
+    global ev, week, one
+    ev = False
+    week = False
+    one = False
+    type_mes = message.text
+    await state.update_data(type=message.text)
     await state.set_state(Form_task.date)
-    if message.answer == 'Ежедневно':
-        await message.answer('Ваша напоминалка готова!')
+    if type_mes == 'Ежедневно':
+        ev = True
+        await state.set_state(Form_task.date)
+    if type_mes == 'В определённый день недели':
+        await state.set_state(Form_task.days)
+        await message.answer('Выберете дни недели')  # здесь должна быть клавиатура
+    if type_mes == 'В один определённый день':
+        await state.set_state(Form_task.date)
+        await message.answer('Выберете день')  # здесь должна быть клавиатура
 
+
+
+@router.message(Form_task.date)
+async def get_date(message: Message, state: FSMContext):
+    if message.text == 'В один определённый день':
+        await state.update_data(date=message.text)
+        await state.set_state(Form_task.days)
+    else:
+        await state.update_data(date='None')
+        await state.set_state(Form_task.days)
+
+    @router.message(Form_task.days)
+    async def get_type(message: Message, state: FSMContext):
+        if message.text == 'В определённый день недели':
+            await state.update_data(days=message.text)
+        else:
+            await state.update_data(days='None')
+        data = await state.get_data()
+        id = data['id']
+        task = data['task']
+        time = data['time']
+        type = data['type']
+        date = data['date']
+        days = data['days']
+        await state.clear()
+        user_id = message.from_user.id
+        task_id = scheduler.add_job(napominalka_update,
+                                    trigger='interval',
+                                    seconds=2,
+                                    kwargs={'user_id': user_id, 'bot': bot})
+        id_task = task_id.id
+        cursor.execute('INSERT INTO users (id, task, time, type, date, days, id_task) VALUES (?,?,?,?,?,?,?)',
+                       (id, task, time, type, date, days, id_task))
+        con.commit()
+        await message.answer('Ваша напоминалка готова!')
